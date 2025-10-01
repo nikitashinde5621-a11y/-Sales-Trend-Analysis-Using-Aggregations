db80e8c43f1446d5267765c128c97d9859649762
Nikita Shinde <nikitashinde5621@gmail.com>
2025-10-01 12:07:13 +0530

    Add files via upload

---
+++ Task6_Sales_Trend_Analysis.sql
@@ -0,0 +1,9 @@
+-- Task 6: Sales Trend Analysis Using SQL
+SELECT
+    EXTRACT(YEAR FROM order_date) AS year,
+    EXTRACT(MONTH FROM order_date) AS month,
+    SUM(amount) AS total_revenue,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM online_sales
+GROUP BY year, month
+ORDER BY year, month;
